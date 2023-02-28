37ae4e297f149246a30491f467d6dbdd6d29fc2c
harisraharjo <haris.r.putro@gmail.com>
2023-03-01 05:58:16 +0700

    up deps

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import { redirectIfUnauthorized } from "~server/auth"
-import { Zoom } from "./test"
 
 export default async function Page() {
   await redirectIfUnauthorized()
 
   return (
     <>
-      <Zoom />
       <h1>Hello, Next!</h1>
     </>
   )
